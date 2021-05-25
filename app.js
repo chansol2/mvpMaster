2467e9562ebe3054127ba346aa3a5359f9fae1c7
Chansol Lim <email@address.com>
2021-05-25 22:35:35 +0900

    add dropdown menu, add hover/click effect using css and javascript

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const path = require("path");
+const API_rcmd = require("./rcmd.js");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// single product list,
+// 1:bed, 2:living, 3:storage
+var spl_1, spl_2, spl_3;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req,res) {
-  res.render("index")
+app.get("/", async function (req, res) {
+  spl_1 = await API_rcmd.rcmd_1();
+  spl_2 = await API_rcmd.rcmd_2();
+  spl_3 = await API_rcmd.rcmd_3();
+
+  res.render("index", {
+    spl_1: spl_1,
+    spl_2: spl_2,
+    spl_3: spl_3,
+  });
 });
 
-app.listen(8080, function() {
-  console.log("server is running at port 8080")
-})
+app.listen(8080, function () {
+  console.log("server is running at port 8080");
+});
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,5 @@
+// 햄버거 메뉴버튼, 또는 엑스 버튼 누를 시, 모바일 토글 메뉴가 각각 열리고 (열려있는 모든 모바일 메뉴 창)닫히는 로직
+
 $("#hamburger,#escape").on("click", function (event) {
   if (this.id === "hamburger") {
     $(".mobile-toggle-menu-container.main").addClass("open");
@@ -6,6 +8,8 @@ $("#hamburger,#escape").on("click", function (event) {
     $(".mobile-toggle-menu-container.sub-menu").removeClass("open");
   }
 });
+
+// 대분류 아이템 클릭 시, 다음 뎊쓰 뷰로 이동되는 로직
 
 $(".mobile-toggle-menu-column > .item").on("click", function (event) {
   event.stopPropagation();
@@ -41,7 +45,53 @@ $(".mobile-toggle-menu-column > .item").on("click", function (event) {
   }
 });
 
+// 뒤로가기 버튼 눌렀을 시, 열려있는 중간 뎁쓰 창 닫는 로직
+
 $(".back").on("click", function (event) {
   target = this.id;
   $(".mobile-toggle-menu-container.sub-menu." + target).removeClass("open");
+});
+
+// 카테고리바에 a 버튼을 눌렀을 시, 해당 드롭다운 매뉴를 열어주는 로직
+
+$(".toggle-dropdown").on("click", function (event) {
+  event.stopPropagation();
+
+  $target = $(".ff").children();
+  $target.removeClass("active");
+  console.log($target);
+
+  console.log(!$($(this).parent()).hasClass("active"));
+  if (!$($(this).parent()).hasClass("active")) {
+    $($(this).parent()).addClass("active");
+  } else {
+    console.log("error");
+  }
+});
+
+$(document).on("click", function (event) {
+  event.stopPropagation();
+  console.log(event.target);
+  if (!$(event.target).hasClass("dropdown-id")) {
+    $target = $(".ff").children();
+    $target.removeClass("active");
+    $("a").removeClass("active");
+  }
+});
+
+$("a").on("click", function (event) {
+  event.stopPropagation();
+  $("a").removeClass("active");
+  $(event.target).addClass("active");
+});
+
+$(".rcmd-large").on("click", function (event) {
+  event.stopPropagation();
+  $("a").removeClass("active");
+  $($(event.currentTarget).children()[1]).addClass("active");
+});
+
+$(".rcmd-middle a").on("click", function (event) {
+  event.stopPropagation();
+  console.log(event);
 });
